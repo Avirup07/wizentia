02444926e7ac99d0680c163e29469841289a2f4d
Avirup07 <sarmisthapal29@gmail.com>
2025-07-15 12:10:14 +0530

    next.js app setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  allowedDevOrigins: ["http://192.168.136.166:3000"],
+};
 
 export default nextConfig;
